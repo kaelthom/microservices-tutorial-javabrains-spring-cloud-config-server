4e891dcb29816b18471d64b8e2d3eb1d1f980ebe
Mickael Thomere <mickael.thomere@gmail.com>
2021-12-19 18:16:20 +0100

    Microservices Tutorial : create Spring Cloud Config Server

---
+++ src/test/java/com/microservicestutorial/moviespringcloudconfigserver/MovieSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicestutorial.moviespringcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieSpringCloudConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
